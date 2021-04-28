8aec2cff22d1109af7254c164ab3fde53724dba5
itom-qe <79703359+itom-qe@users.noreply.github.com>
2021-04-28 15:10:09 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -14,6 +14,6 @@ variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
 }
 
-variable "location" {
+variable "region" {
   description = "The Azure Region in which all resources in this example should be created."
 }
