caff15c045d7142046926c777581d6d73042cc1c
itom-qe <79703359+itom-qe@users.noreply.github.com>
2021-04-28 15:09:50 +0530

    Update template.tf

--- template.tf
+++ template.tf
@@ -1,12 +1,12 @@
 resource "azurerm_resource_group" "example" {
   name     = "${var.prefix}-resources"
-  location = "${var.location}"
+  location = "${var.region}"
 }
 
 resource "azurerm_sql_server" "example" {
   name                         = "${var.prefix}-sqlsvr"
   resource_group_name          = "${azurerm_resource_group.example.name}"
-  location                     = "${azurerm_resource_group.example.location}"
+  location                     = "${azurerm_resource_group.example.region}"
   version                      = "12.0"
   administrator_login          = "4dm1n157r470r"
   administrator_login_password = "4-v3ry-53cr37-p455w0rd"
@@ -15,7 +15,7 @@ resource "azurerm_sql_server" "example" {
 resource "azurerm_sql_database" "example" {
   name                             = "${var.prefix}-db"
   resource_group_name              = "${azurerm_resource_group.example.name}"
-  location                         = "${azurerm_resource_group.example.location}"
+  location                         = "${azurerm_resource_group.example.region}"
   server_name                      = "${azurerm_sql_server.example.name}"
   edition                          = "Basic"
   collation                        = "SQL_Latin1_General_CP1_CI_AS"
