ef08ee6662b5c963c986eea13a35cf3f9c46a2bd
itom-qe <79703359+itom-qe@users.noreply.github.com>
2021-04-28 14:35:48 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,3 +1,15 @@
+variable "subscriptionId" {
+}
+
+variable "clientId" {
+}
+
+variable "clientSecret" {
+}
+
+variable "tenantId" {
+}
+
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
 }
